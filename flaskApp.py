bc9e6ae2c9433c17c82c5a7036ddafc0e2af6507
root <root@DESKTOP-7U1SCBV.localdomain>
2018-01-04 09:17:06 -0600

    Fixed a bug

--- flaskApp.py
+++ flaskApp.py
@@ -1,14 +1,26 @@
 # my attempt at building my markov chain using flask
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request
 import markovfy
 app = Flask(__name__)
 
+class mchain(object):
+    def __init__(self):
+        self.markov = markovfy.markov()
+    def yieldTweet(self):
+        return self.markov.getText()
+
+init = mchain()
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template("landing_page.html", tweet= init.yieldTweet())
 
-    text = markovfy.markov().getText()
-    return render_template("landing_page.html", tweet=text)
+@app.route('/getTweet', methods=['GET'])
+def getTweet():
+    print("Calling getTweet()")
+    with app.test_request_context('/getTweet', method='GET'):
+        return init.yieldTweet()
+
 
 if __name__ == "__main__":
     app.run()
